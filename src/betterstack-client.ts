2adb6a11eed51e35c12b52b6c62de4f20a1daf09
Faiyam Rahman <faiyam@faiyamrahman.com>
2025-07-25 19:08:27 -0700

    fix typescript

--- src/betterstack-client.ts
+++ src/betterstack-client.ts
@@ -549,7 +549,7 @@ export class BetterstackClient {
       logToFile('INFO', 'ClickHouse API response received', {
         keys: Object.keys(response),
         data: response.data,
-        exception: response.exception,
+        exception: (response as any).exception,
         status: response.status,
         statusText: response.statusText,
         dataLength: response.data?.data?.length || response.data?.length || 0,
